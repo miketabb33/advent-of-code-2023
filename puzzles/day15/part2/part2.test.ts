140caf9e02745493cdbe6f0cf7dbaac32fed9b93
miketabb33 <miketabb33@gmail.com>
2024-01-12 08:13:52 -0500

    Day 15: complete part 2

--- puzzles/day15/part2/part2.test.ts
+++ puzzles/day15/part2/part2.test.ts
@@ -1,15 +1,68 @@
 import { describe, expect, it } from 'bun:test'
-import { day15Part2, makeBoxSeries } from './part2'
+import {
+  Lense,
+  LenseRemoval,
+  day15Part2,
+  hashAlgorithm,
+  makeBoxSeries,
+  parseStep,
+  processLenseSteps,
+} from './part2'
 
 describe('Day15Part2', () => {
-  it('should calc example', () => {
+  it('should', () => {
     const input = 'rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7'
-    day15Part2(input)
+    expect(day15Part2(input)).toEqual(145)
+  })
+})
+
+describe('Process Lense Steps', () => {
+  it('should add lenses', () => {
+    const result = processLenseSteps(['rn=1', 'qp=3', 'cm=2'])
+    expect(result[0].length).toEqual(2)
+    expect(result[0][0].id).toEqual('rn')
+    expect(result[0][1].id).toEqual('cm')
+    expect(result[1][0].id).toEqual('qp')
+    expect(result[1][0].focalLength).toEqual(3)
+  })
+
+  it('should update lense when exists', () => {
+    const result = processLenseSteps(['rn=1', 'rn=5'])
+    expect(result[0].length).toEqual(1)
+    expect(result[0][0].focalLength).toEqual(5)
+  })
+
+  it('should remove lenses', () => {
+    const result = processLenseSteps(['rn=1', 'cm=5', 'rn-'])
+    expect(result[0].length).toEqual(1)
+    expect(result[0][0].id).toEqual('cm')
   })
 })
 
 describe('Make Box Series', () => {
   it('should be correct', () => {
     expect(makeBoxSeries().length).toEqual(256)
+  })
+})
+
+describe('Hash Algorithm', () => {
+  it('should be correct', () => {
+    expect(hashAlgorithm('qp')).toEqual(1)
+    expect(hashAlgorithm('pc')).toEqual(3)
+  })
+})
+
+describe('Parse Step', () => {
+  it('should parse lense', () => {
+    const result = parseStep('rn=1') as Lense
+    expect(result.focalLength).toEqual(1)
+    expect(result.operator).toEqual('=')
+    expect(result.id).toEqual('rn')
+  })
+
+  it('should parse lense', () => {
+    const result = parseStep('cm-') as LenseRemoval
+    expect(result.operator).toEqual('-')
+    expect(result.id).toEqual('cm')
   })
 })
--- puzzles/day15/part2/part2.ts
+++ puzzles/day15/part2/part2.ts
@@ -1,17 +1,89 @@
-type Lense = {
+export type LenseRemoval = {
   id: string
-  focalLength: number
+  operator: '-'
 }
 
-export const day15Part2 = (input: string) => {
+export type Lense = {
+  id: string
+  focalLength: number
+  operator: '='
+}
+
+type ParsedStep = LenseRemoval | Lense
+
+export const day15Part2 = (input: string): number => {
   const steps = input.trim().split(',')
+  const boxSeries = processLenseSteps(steps)
+
+  let total = 0
+
+  boxSeries.forEach((box, i) => {
+    let sum = 0
+    box.forEach((lense, j) => {
+      sum += lense.focalLength * (j + 1) * (i + 1)
+    })
+
+    total += sum
+  })
+
+  return total
+}
+
+export const processLenseSteps = (steps: string[]): Lense[][] => {
   const boxSeries = makeBoxSeries()
-  boxSeries[0].push({ id: 'h', focalLength: 3 })
-  console.log(boxSeries[0])
+
+  steps.forEach((step) => {
+    const parsedStep = parseStep(step)
+    const boxId = hashAlgorithm(parsedStep.id)
+
+    if (parsedStep.operator === '=') {
+      const existingLense = boxSeries[boxId].find((x) => x.id === parsedStep.id)
+      if (existingLense) {
+        existingLense.focalLength = parsedStep.focalLength
+      } else {
+        boxSeries[boxId].push(parsedStep)
+      }
+    } else if (parsedStep.operator === '-') {
+      boxSeries[boxId] = boxSeries[boxId].filter((x) => x.id !== parsedStep.id)
+    }
+  })
+
+  return boxSeries
+}
+
+export const parseStep = (step: string): ParsedStep => {
+  if (step.includes('=')) {
+    const idAndFocalLength = step.split('=')
+    return {
+      focalLength: +idAndFocalLength[1],
+      operator: '=',
+      id: idAndFocalLength[0],
+    }
+  }
+
+  if (step.includes('-')) {
+    const id = step.split('-')
+    return {
+      operator: '-',
+      id: id[0],
+    }
+  }
+
+  throw new Error(`${step} is invalid`)
 }
 
 export const makeBoxSeries = (): Lense[][] => {
   const boxSeries: Lense[][] = []
   for (let i = 0; i < 256; i++) boxSeries.push([])
   return boxSeries
+}
+
+export const hashAlgorithm = (step: string): number => {
+  let currentValue = 0
+  step.split('').forEach((char) => {
+    currentValue += char.charCodeAt(0)
+    currentValue *= 17
+    currentValue %= 256
+  })
+  return currentValue
 }
