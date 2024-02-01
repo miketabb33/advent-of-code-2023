e7512d6740e55096928f09c8006253290c57276e
miketabb33 <miketabb33@gmail.com>
2024-01-31 21:17:21 -0500

    Day 18: draw input into file

--- puzzles/day18/part1/day18Part1.test.ts
+++ puzzles/day18/part1/day18Part1.test.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from 'fs'
 import { describe, expect, it } from 'bun:test'
-import { createDigMap, parseDigPlans } from './day18Part1'
+import { DigMap, parseDigPlans } from './day18Part1'
 
 const input = readFileSync(`${__dirname}/../exampleInput`).toString()
 const lines = input.split('\n').filter((x) => !!x)
@@ -21,36 +21,81 @@ describe('Parse Dig Plan', () => {
   })
 })
 
-describe('Create Dig Map', () => {
-  it('should create it', () => {
-    const result = createDigMap(digPlans)
-    expect(result.length).toEqual(38)
+describe('Dig Map', () => {
+  describe('Create Dig Map', () => {
+    it('should create it', () => {
+      const result = DigMap.make(digPlans).getMap()
+      expect(result.length).toEqual(38)
 
-    expect(result[0]).toEqual({ x: 0, y: 0 })
-    expect(result[1]).toEqual({ x: 1, y: 0 })
-    expect(result[2]).toEqual({ x: 2, y: 0 })
-    expect(result[3]).toEqual({ x: 3, y: 0 })
-    expect(result[4]).toEqual({ x: 4, y: 0 })
-    expect(result[5]).toEqual({ x: 5, y: 0 })
-    expect(result[6]).toEqual({ x: 5, y: 1 })
-    expect(result[7]).toEqual({ x: 5, y: 2 })
-    expect(result[8]).toEqual({ x: 5, y: 3 })
-    expect(result[9]).toEqual({ x: 5, y: 4 })
-    expect(result[10]).toEqual({ x: 5, y: 5 })
-    expect(result[11]).toEqual({ x: 4, y: 5 })
-    expect(result[12]).toEqual({ x: 3, y: 5 })
-    expect(result[13]).toEqual({ x: 3, y: 6 })
-    expect(result[14]).toEqual({ x: 3, y: 7 })
-    expect(result[15]).toEqual({ x: 4, y: 7 })
-    expect(result[16]).toEqual({ x: 5, y: 7 })
-    expect(result[17]).toEqual({ x: 5, y: 8 })
-    expect(result[18]).toEqual({ x: 5, y: 9 })
-    expect(result[19]).toEqual({ x: 4, y: 9 })
-    expect(result[20]).toEqual({ x: 3, y: 9 })
-    expect(result[21]).toEqual({ x: 2, y: 9 })
-    expect(result[22]).toEqual({ x: 1, y: 9 })
-    expect(result[23]).toEqual({ x: 0, y: 9 })
-    expect(result[24]).toEqual({ x: 0, y: 8 })
-    expect(result[25]).toEqual({ x: 0, y: 7 })
+      expect(result[0]).toEqual({ x: 0, y: 0 })
+      expect(result[1]).toEqual({ x: 1, y: 0 })
+      expect(result[2]).toEqual({ x: 2, y: 0 })
+      expect(result[3]).toEqual({ x: 3, y: 0 })
+      expect(result[4]).toEqual({ x: 4, y: 0 })
+      expect(result[5]).toEqual({ x: 5, y: 0 })
+      expect(result[6]).toEqual({ x: 5, y: 1 })
+      expect(result[7]).toEqual({ x: 5, y: 2 })
+      expect(result[8]).toEqual({ x: 5, y: 3 })
+      expect(result[9]).toEqual({ x: 5, y: 4 })
+      expect(result[10]).toEqual({ x: 5, y: 5 })
+      expect(result[11]).toEqual({ x: 4, y: 5 })
+      expect(result[12]).toEqual({ x: 3, y: 5 })
+      expect(result[13]).toEqual({ x: 3, y: 6 })
+      expect(result[14]).toEqual({ x: 3, y: 7 })
+      expect(result[15]).toEqual({ x: 4, y: 7 })
+      expect(result[16]).toEqual({ x: 5, y: 7 })
+      expect(result[17]).toEqual({ x: 5, y: 8 })
+      expect(result[18]).toEqual({ x: 5, y: 9 })
+      expect(result[19]).toEqual({ x: 4, y: 9 })
+      expect(result[20]).toEqual({ x: 3, y: 9 })
+      expect(result[21]).toEqual({ x: 2, y: 9 })
+      expect(result[22]).toEqual({ x: 1, y: 9 })
+      expect(result[23]).toEqual({ x: 0, y: 9 })
+      expect(result[24]).toEqual({ x: 0, y: 8 })
+      expect(result[25]).toEqual({ x: 0, y: 7 })
+    })
+  })
+
+  describe('Box', () => {
+    it('should get box', () => {
+      const results = DigMap.make(digPlans).getBox()
+      expect(results).toEqual({ minX: -1, minY: 0, maxX: 5, maxY: 9 })
+    })
+  })
+
+  describe('Get Matrix', () => {
+    it('should get matrix', () => {
+      const results = DigMap.make(digPlans).getBlankMatrix()
+      expect(results[0].length).toEqual(7)
+      expect(results.length).toEqual(10)
+      expect(results[2][5].position).toEqual({ x: 4, y: 2 })
+    })
+  })
+
+  describe('Print', () => {
+    it('should print', () => {
+      const digMap = DigMap.make(digPlans)
+      const results = digMap.print(digMap.getBlankMatrix())
+      expect(results[0].length).toEqual(7)
+      expect(results.length).toEqual(10)
+    })
+  })
+
+  it('get it', () => {
+    const examplePrint = [
+      '#######',
+      '#.....#',
+      '###...#',
+      '..#...#',
+      '..#...#',
+      '###.###',
+      '#...#..',
+      '##..###',
+      '.#....#',
+      '.######',
+    ]
+    const digMap = DigMap.make(digPlans)
+    const matrix = digMap.getDigPlanMatrix()
+    expect(digMap.print(matrix)).toEqual(examplePrint)
   })
 })
--- puzzles/day18/part1/day18Part1.ts
+++ puzzles/day18/part1/day18Part1.ts
@@ -1,5 +1,19 @@
+import { writeFileSync } from 'fs'
+
 type Direction = 'up' | 'right' | 'down' | 'left'
 type Position = { x: number; y: number }
+
+type Frame = {
+  position: Position
+  value: string
+}
+
+type Box = {
+  minX: number
+  minY: number
+  maxX: number
+  maxY: number
+}
 
 type DigPlanItem = {
   direction: Direction
@@ -7,18 +21,108 @@ type DigPlanItem = {
   color: string
 }
 
-class DigMap {
+export class DigMap {
   private currentDigMap: Position[] = []
-  current = () => this.currentDigMap[this.currentDigMap.length - 1]
-  push = (position: Position) => this.currentDigMap.push(position)
+  private box: Box = { minX: 0, minY: 0, maxX: 0, maxY: 0 }
+
+  static make = (digPlan: DigPlanItem[]) => {
+    const digMap = new DigMap()
+    digPlan.forEach((item) => DigMap.processDigPlanItem(item, digMap))
+    digMap.box = digMap.calculateBox()
+    return digMap
+  }
+
+  static processDigPlanItem = (item: DigPlanItem, digMap: DigMap) => {
+    for (let i = 0; i < item.meters; i++) {
+      digMap.push(DigMap.getNextPosition(item.direction, digMap.current()))
+    }
+  }
+
+  static getNextPosition = (direction: Direction, lastPosition: Position) => {
+    if (!lastPosition) return { x: 0, y: 0 }
+    if (direction === 'right')
+      return { x: lastPosition.x + 1, y: lastPosition.y }
+    if (direction === 'down')
+      return { x: lastPosition.x, y: lastPosition.y + 1 }
+    if (direction === 'left')
+      return { x: lastPosition.x - 1, y: lastPosition.y }
+    if (direction === 'up') return { x: lastPosition.x, y: lastPosition.y - 1 }
+    throw new Error('unsupported direction: "' + direction + '"')
+  }
+
   getMap = () => this.currentDigMap
+  getBox = () => this.box
+
+  getBlankMatrix = () => {
+    const matrix: Frame[][] = []
+    let row = 0
+    let currentY = this.box.minY
+    const height = Math.abs(this.box.minY) + Math.abs(this.box.maxY)
+    while (row <= height) {
+      matrix[row] = []
+      for (let i = this.box.minX; i <= this.box.maxX; i++) {
+        matrix[row].push({ position: { x: i, y: currentY }, value: '.' })
+      }
+      row++
+      currentY++
+    }
+
+    return matrix
+  }
+
+  getDigPlanMatrix = () => {
+    const matrix = this.getBlankMatrix()
+    const rows = matrix.map((f) => f.map((f) => f.position.y)[0])
+
+    this.currentDigMap.forEach((item) => {
+      const row = rows.findIndex((x) => x === item.y)
+      const i = matrix[row].findIndex((x) => x.position.x === item.x)
+      matrix[row][i].value = '#'
+    })
+    return matrix
+  }
+
+  print = (matrix: Frame[][]) => {
+    const lines: string[] = []
+
+    matrix.forEach((frames) => {
+      let line = ''
+      frames.forEach((frame) => {
+        line += frame.value
+      })
+      lines.push(line)
+    })
+    return lines
+  }
+
+  private current = () => this.currentDigMap[this.currentDigMap.length - 1]
+  private push = (position: Position) => this.currentDigMap.push(position)
+
+  private calculateBox(): Box {
+    let minX = 0
+    let minY = 0
+    let maxX = 0
+    let maxY = 0
+
+    this.currentDigMap.forEach((p) => {
+      if (p.x < minX) minX = p.x
+      if (p.y < minY) minY = p.y
+      if (p.x > maxX) maxX = p.x
+      if (p.y > maxY) maxY = p.y
+    })
+
+    return { minX, minY, maxX, maxY }
+  }
 }
 
 export const day18Part1 = (input: string) => {
   const lines = input.split('\n').filter((x) => !!x)
   const digPlan = parseDigPlans(lines)
-  const digMap = createDigMap(digPlan)
-  return `Ready, solve that puzzle! Input: ${lines}`
+  const digMap = DigMap.make(digPlan)
+  const printed = digMap.print(digMap.getDigPlanMatrix())
+
+  writeFileSync(`${__dirname}/../printed`, printed.join('\n'))
+  return ''
 }
 
 export const parseDigPlans = (lines: string[]): DigPlanItem[] => {
@@ -36,26 +140,4 @@ export const parseDigPlans = (lines: string[]): DigPlanItem[] => {
       color: parts[2],
     }
   })
-}
-
-export const createDigMap = (digPlan: DigPlanItem[]): Position[] => {
-  const digMap = new DigMap()
-  digPlan.forEach((item) => processDigPlanItem(item, digMap))
-
-  return digMap.getMap()
-}
-
-const processDigPlanItem = (item: DigPlanItem, digMap: DigMap) => {
-  for (let i = 0; i < item.meters; i++) {
-    digMap.push(getNextPosition(item.direction, digMap.current()))
-  }
-}
-
-const getNextPosition = (direction: Direction, lastPosition: Position) => {
-  if (!lastPosition) return { x: 0, y: 0 }
-  if (direction === 'right') return { x: lastPosition.x + 1, y: lastPosition.y }
-  if (direction === 'down') return { x: lastPosition.x, y: lastPosition.y + 1 }
-  if (direction === 'left') return { x: lastPosition.x - 1, y: lastPosition.y }
-  if (direction === 'up') return { x: lastPosition.x, y: lastPosition.y - 1 }
-  throw new Error('unsupported direction: "' + direction + '"')
 }
