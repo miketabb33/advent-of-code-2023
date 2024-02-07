cb9eddbb85292ce0bb3e17ce2a0ff60b1ee8a17a
miketabb33 <miketabb33@gmail.com>
2024-02-06 21:36:01 -0500

    Day 19: part 1 and 2

--- puzzles/day19/part1/part1.test.ts
+++ puzzles/day19/part1/part1.test.ts
@@ -1,9 +1,22 @@
 import { describe, expect, it } from 'bun:test'
 import { readFileSync } from 'fs'
-import { ConditionalRule, parseWorkflows, splitParts } from './part1'
+import {
+  ConditionalRule,
+  WorkflowPart,
+  day19Part1,
+  parseWorkflows,
+  splitParts,
+} from './part1'
 
 const input = readFileSync(`${__dirname}/../exampleInput`).toString()
 const parts = splitParts(input)
+const workflows = parseWorkflows(parts[0])
+
+describe('Day 19 part 1', () => {
+  it('should run example', () => {
+    expect(day19Part1(input)).toEqual(19114)
+  })
+})
 
 describe('should parse', () => {
   it('split parts', () => {
@@ -13,8 +26,67 @@ describe('should parse', () => {
   })
 
   it('should parse workflows', () => {
-    const results = parseWorkflows(parts[0])
-    const result1 = results.get('px')
-    expect(result1?.length).toEqual(3)
+    const result1 = workflows.get('px')!
+    expect(result1.length).toEqual(3)
+
+    expect(result1[0].type).toEqual('conditional')
+    expect((result1[0] as ConditionalRule).category).toEqual('a')
+    expect((result1[0] as ConditionalRule).operation).toEqual('<')
+    expect((result1[0] as ConditionalRule).value).toEqual(2006)
+    expect((result1[0] as ConditionalRule).dest).toEqual('qkq')
+
+    expect(result1[1].type).toEqual('conditional')
+    expect((result1[1] as ConditionalRule).category).toEqual('m')
+    expect((result1[1] as ConditionalRule).operation).toEqual('>')
+    expect((result1[1] as ConditionalRule).value).toEqual(2090)
+    expect((result1[1] as ConditionalRule).dest).toEqual('A')
+
+    expect(result1[2].type).toEqual('direct')
+    expect(result1[2].dest).toEqual('rfg')
+
+    const result2 = workflows.get('in')!
+    expect(result2.length).toEqual(2)
+
+    expect(result2[0].type).toEqual('conditional')
+    expect((result2[0] as ConditionalRule).category).toEqual('s')
+    expect((result2[0] as ConditionalRule).operation).toEqual('<')
+    expect((result2[0] as ConditionalRule).value).toEqual(1351)
+    expect((result2[0] as ConditionalRule).dest).toEqual('px')
+
+    expect(result2[1].type).toEqual('direct')
+    expect(result2[1].dest).toEqual('qqz')
+  })
+})
+
+describe('Workflow Part', () => {
+  it('should get sum for 1', () => {
+    const subject = new WorkflowPart(parts[1][0])
+    expect(subject.sum()).toEqual(7540)
+  })
+  it('should get sum for 2', () => {
+    const subject = new WorkflowPart(parts[1][1])
+    expect(subject.sum()).toEqual(4286)
+  })
+
+  //{x=787,m=2655,a=1222,s=2876}
+  it('is Accepted - 1', () => {
+    const subject = new WorkflowPart(parts[1][0])
+    expect(subject.isAccepted(workflows)).toBe(true)
+  })
+  it('is Accepted - 2', () => {
+    const subject = new WorkflowPart(parts[1][1])
+    expect(subject.isAccepted(workflows)).toBe(false)
+  })
+  it('is Accepted - 3', () => {
+    const subject = new WorkflowPart(parts[1][2])
+    expect(subject.isAccepted(workflows)).toBe(true)
+  })
+  it('is Accepted - 4', () => {
+    const subject = new WorkflowPart(parts[1][3])
+    expect(subject.isAccepted(workflows)).toBe(false)
+  })
+  it('is Accepted - 5', () => {
+    const subject = new WorkflowPart(parts[1][4])
+    expect(subject.isAccepted(workflows)).toBe(true)
   })
 })
--- puzzles/day19/part1/part1.ts
+++ puzzles/day19/part1/part1.ts
@@ -1,7 +1,13 @@
+type Category = 'x' | 'm' | 'a' | 's'
+type Operation = '<' | '>'
+
+type Workflows = Map<string, (ConditionalRule | DirectRule)[]>
+
 export type ConditionalRule = {
   type: 'conditional'
-  value: string
-  operation: '<' | '>'
+  category: Category
+  value: number
+  operation: Operation
   dest: string
 }
 
@@ -10,9 +16,18 @@ export type DirectRule = {
   dest: string
 }
 
-export const starter = (input: string) => {
-  const lines = input.split('\n').filter((x) => !!x)
-  return `Ready, solve that puzzle! Input: ${lines}`
+export const day19Part1 = (input: string): number => {
+  const parts = splitParts(input)
+  const workflows = parseWorkflows(parts[0])
+  let sum = 0
+
+  parts[1].forEach((partString) => {
+    const part = new WorkflowPart(partString)
+    const isAccepted = part.isAccepted(workflows)
+    if (isAccepted) sum += part.sum()
+  })
+
+  return sum
 }
 
 export const splitParts = (input: string) => {
@@ -23,18 +38,83 @@ export const splitParts = (input: string) => {
   ]
 }
 
-export const parseWorkflows = (
-  lines: string[]
-): Map<string, (ConditionalRule | DirectRule)[]> => {
+export const parseWorkflows = (lines: string[]): Workflows => {
   const workflows = new Map<string, (ConditionalRule | DirectRule)[]>()
 
   lines.forEach((line) => {
-    workflows.set('px', [parseRule(), parseRule(), parseRule()])
+    const nameAndRules = line.replace('}', '').split('{')
+    const rules = parseRules(nameAndRules[1])
+    workflows.set(nameAndRules[0], rules)
   })
 
   return workflows
 }
 
-const parseRule = (): DirectRule => {
-  return { type: 'direct', dest: '' }
+const parseRules = (rulesLine: string): (ConditionalRule | DirectRule)[] => {
+  const rulesString = rulesLine.split(',')
+
+  return rulesString.map((ruleString) => {
+    const colonIndex = ruleString.indexOf(':')
+    if (colonIndex != -1) {
+      const conditionalRule: ConditionalRule = {
+        type: 'conditional',
+        category: ruleString[0] as Category,
+        value: +ruleString.substring(2, colonIndex),
+        operation: ruleString[1] as Operation,
+        dest: ruleString.substring(colonIndex + 1, ruleString.length),
+      }
+      return conditionalRule
+    } else {
+      return { type: 'direct', dest: ruleString }
+    }
+  })
+}
+
+export class WorkflowPart {
+  private values = new Map<Category, number>()
+  private currentWorkflow = 'in'
+
+  constructor(partString: string) {
+    const valueString = partString.replace('}', '').replace('{', '').split(',')
+    this.values.set('x', this.getValue(valueString[0]))
+    this.values.set('m', this.getValue(valueString[1]))
+    this.values.set('a', this.getValue(valueString[2]))
+    this.values.set('s', this.getValue(valueString[3]))
+  }
+
+  private getValue = (section: string): number => {
+    return +section.substring(2, section.length)
+  }
+
+  isAccepted = (workflow: Workflows): boolean => {
+    while (this.currentWorkflow !== 'A' && this.currentWorkflow !== 'R') {
+      const rules = workflow.get(this.currentWorkflow)!
+
+      for (let i = 0; i < rules.length; i++) {
+        const rule = rules[i]
+        if (this.isValidDest(rule)) {
+          this.currentWorkflow = rule.dest
+          break
+        }
+      }
+    }
+
+    return this.currentWorkflow === 'A'
+  }
+  private isValidDest = (rule: ConditionalRule | DirectRule) => {
+    if (rule.type === 'conditional') {
+      const comp = this.values.get(rule.category)!
+      if (rule.operation === '<') return comp < rule.value
+      if (rule.operation === '>') return comp > rule.value
+    } else return true
+  }
+
+  sum = () => {
+    return (
+      this.values.get('x')! +
+      this.values.get('m')! +
+      this.values.get('a')! +
+      this.values.get('s')!
+    )
+  }
 }
