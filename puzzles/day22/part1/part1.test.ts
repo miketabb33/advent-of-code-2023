376a0d0e41019631d99d7d5c92721a33732eb482
miketabb33 <miketabb33@gmail.com>
2024-02-24 12:45:00 -0500

    Day 22: part 1 progress

--- puzzles/day22/part1/part1.test.ts
+++ puzzles/day22/part1/part1.test.ts
@@ -1,7 +1,63 @@
 import { describe, expect, it } from 'bun:test'
+import { readFileSync } from 'fs'
+import { dropBricks, parseBrickInput } from './part1'
 
-describe('', () => {
-  it('', () => {
-    expect(true).toEqual(false)
+const input = readFileSync(`${__dirname}/../exampleInput`).toString()
+const lines = input.split('\n').filter((x) => !!x)
+const parsedBricks = parseBrickInput(lines)
+
+describe('drop bricks', () => {
+  it('should drop', () => {
+    const result = dropBricks(parsedBricks)
+    console.log(result)
+  })
+})
+
+describe('Parse Brick Input', () => {
+  it('should return empty arr when given an arr', () => {
+    const result = parseBrickInput([]).bricks
+    expect(result).toEqual([])
+  })
+
+  it('should parse', () => {
+    const result = parseBrickInput(lines).bricks
+    expect(parseBrickInput(lines).size).toEqual(2)
+    expect(result.length).toEqual(7)
+
+    expect(result[0].id).toEqual(0)
+    expect(result[0].coord1.x).toEqual(1)
+    expect(result[0].coord1.y).toEqual(0)
+    expect(result[0].coord1.z).toEqual(1)
+    expect(result[0].coord2.x).toEqual(1)
+    expect(result[0].coord2.y).toEqual(2)
+    expect(result[0].coord2.z).toEqual(1)
+
+    expect(result[1].id).toEqual(1)
+    expect(result[1].coord1.x).toEqual(0)
+    expect(result[1].coord1.y).toEqual(0)
+    expect(result[1].coord1.z).toEqual(2)
+    expect(result[1].coord2.x).toEqual(2)
+    expect(result[1].coord2.y).toEqual(0)
+    expect(result[1].coord2.z).toEqual(2)
+
+    expect(result[2].id).toEqual(2)
+    expect(result[2].coord1.x).toEqual(0)
+    expect(result[2].coord1.y).toEqual(2)
+    expect(result[2].coord1.z).toEqual(3)
+    expect(result[2].coord2.x).toEqual(2)
+    expect(result[2].coord2.y).toEqual(2)
+    expect(result[2].coord2.z).toEqual(3)
+
+    expect(result[3].id).toEqual(3)
+    expect(result[3].coord1.z).toEqual(4)
+
+    expect(result[4].id).toEqual(4)
+    expect(result[4].coord1.z).toEqual(5)
+
+    expect(result[5].id).toEqual(5)
+    expect(result[5].coord1.z).toEqual(6)
+
+    expect(result[6].id).toEqual(6)
+    expect(result[6].coord1.z).toEqual(8)
   })
 })
--- puzzles/day22/part1/part1.ts
+++ puzzles/day22/part1/part1.ts
@@ -1,4 +1,78 @@
+type Coordinate = {
+  x: number
+  y: number
+  z: number
+}
+
+type Brick = {
+  id: number
+  coord1: Coordinate
+  coord2: Coordinate
+}
+
+type BrickParseResults = {
+  bricks: Brick[]
+  size: number
+}
+
 export const starter = (input: string) => {
   const lines = input.split('\n').filter((x) => !!x)
-  return `Ready, solve that puzzle! Input: ${lines}`
+  const parsedBricks = parseBrickInput(lines)
+  const settledBricks = dropBricks(parsedBricks)
+
+  return []
+}
+
+export const dropBricks = (parsedBricks: BrickParseResults) => {
+  const bricks = parsedBricks.bricks
+  const stackTopography = initTopography(parsedBricks.size)
+  const stack: Brick[] = []
+
+  // using the topography and current block, check if the current block can drop
+  // if it can, update the blocks z indexes to the new dropped position
+  // finally, update the topography to include the current blocks topography
+
+  console.log(bricks[0])
+
+  console.log(stackTopography)
+}
+
+const initTopography = (size: number) => {
+  const stackTopography: Coordinate[] = []
+  for (let i = 0; i <= size; i++) {
+    for (let j = 0; j <= size; j++) {
+      stackTopography.push({ x: j, y: i, z: 0 })
+    }
+  }
+  return stackTopography
+}
+
+export const parseBrickInput = (lines: string[]): BrickParseResults => {
+  let size = 0
+
+  const bricksCoords = lines.map((line) => {
+    const oneAndTwo = line.split('~')
+    const coord1 = oneAndTwo[0].split(',')
+    const coord2 = oneAndTwo[1].split(',')
+
+    const maxXY = getMaxXY(coord1, coord2)
+    if (size < maxXY) size = maxXY
+
+    const brick: [Coordinate, Coordinate] = [
+      { x: +coord1[0], y: +coord1[1], z: +coord1[2] },
+      { x: +coord2[0], y: +coord2[1], z: +coord2[2] },
+    ]
+    return brick
+  })
+  const sortedBrickCoords = bricksCoords.sort((a, b) => a[0].z - b[0].z)
+  const bricks = sortedBrickCoords.map((coords, i) => {
+    const brick: Brick = { id: i, coord1: coords[0], coord2: coords[1] }
+    return brick
+  })
+
+  return { bricks, size }
+}
+
+const getMaxXY = (coord1: string[], coord2: string[]): number => {
+  return Math.max(+coord1[0], +coord1[1], +coord2[0], +coord2[1])
 }
