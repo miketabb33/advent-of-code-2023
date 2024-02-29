803958d3e08f42b11536727bdc509eda7bd6a50e
miketabb33 <miketabb33@gmail.com>
2024-02-28 20:33:42 -0500

    Day 23: complete part 1

--- puzzles/day23/part1/part1.test.ts
+++ puzzles/day23/part1/part1.test.ts
@@ -1,7 +1,103 @@
 import { describe, expect, it } from 'bun:test'
+import { readFileSync } from 'fs'
+import { Current, MazeMap, Point, day23Part1 } from './part1'
 
-describe('', () => {
-  it('', () => {
-    expect(true).toEqual(false)
+const input = readFileSync(`${__dirname}/../exampleInput`).toString()
+const map = new MazeMap(input)
+const startPoint: Point = { x: 1, y: 0 }
+
+describe('Start', () => {
+  expect(day23Part1(input)).toEqual(94)
+})
+
+describe('Maze Map', () => {
+  it('should get for position 0,0', () => {
+    expect(map.getItem({ x: 0, y: 0 })).toEqual('#')
+  })
+  it('should get for position 1,0', () => {
+    expect(map.getItem({ x: 1, y: 0 })).toEqual('.')
+  })
+  it('should get for position 10,3', () => {
+    expect(map.getItem({ x: 10, y: 3 })).toEqual('>')
+  })
+})
+
+describe('Find Next Points', () => {
+  it('should find first', () => {
+    const results1 = map.findNextPoints(startPoint)
+    expect(results1.length).toEqual(1)
+    expect(results1[0]).toEqual({ point: { x: 1, y: 1 }, value: '.' })
+
+    const results2 = map.findNextPoints(results1[0].point)
+    expect(results2[1]).toEqual({ point: { x: 2, y: 1 }, value: '.' })
+  })
+
+  it('should find next gap', () => {
+    const results1 = map.findNextPoints({ x: 7, y: 1 })
+    expect(results1[0]).toEqual({ point: { x: 7, y: 2 }, value: '.' })
+
+    const results2 = map.findNextPoints({ x: 7, y: 2 })
+    expect(results2[0]).toEqual({ point: { x: 7, y: 3 }, value: '.' })
+
+    const results3 = map.findNextPoints({ x: 7, y: 3 })
+    expect(results3[1]).toEqual({ point: { x: 6, y: 3 }, value: '.' })
+
+    const results4 = map.findNextPoints({ x: 6, y: 3 })
+    expect(results4[0]).toEqual({ point: { x: 7, y: 3 }, value: '.' })
+    expect(results4[1]).toEqual({ point: { x: 5, y: 3 }, value: '.' })
+
+    const results5 = map.findNextPoints({ x: 9, y: 11 })
+    expect(results5[0]).toEqual({ point: { x: 9, y: 10 }, value: '.' })
+    expect(results5[1]).toEqual({ point: { x: 8, y: 11 }, value: '.' })
+  })
+})
+
+describe('Current', () => {
+  it('should return starter', () => {
+    const current = new Current()
+    expect(current.point()).toEqual({ x: 1, y: 0 })
+  })
+
+  it('should move to the next valid spot', () => {
+    const current = new Current()
+    current.move(map.findNextPoints(current.point()))
+    expect(current.point()).toEqual({ x: 1, y: 1 })
+
+    current.move(map.findNextPoints(current.point()))
+    expect(current.point()).toEqual({ x: 2, y: 1 })
+
+    current.move(map.findNextPoints(current.point()))
+    expect(current.point()).toEqual({ x: 3, y: 1 })
+
+    current.move(map.findNextPoints(current.point()))
+    expect(current.point()).toEqual({ x: 4, y: 1 })
+
+    current.move(map.findNextPoints(current.point()))
+    expect(current.point()).toEqual({ x: 5, y: 1 })
+
+    current.move(map.findNextPoints(current.point()))
+    expect(current.point()).toEqual({ x: 6, y: 1 })
+
+    current.move(map.findNextPoints(current.point()))
+    expect(current.point()).toEqual({ x: 7, y: 1 })
+
+    current.move(map.findNextPoints(current.point()))
+    expect(current.point()).toEqual({ x: 7, y: 2 })
+
+    current.move(map.findNextPoints(current.point()))
+    expect(current.point()).toEqual({ x: 7, y: 3 })
+
+    current.move(map.findNextPoints(current.point()))
+    expect(current.point()).toEqual({ x: 6, y: 3 })
+  })
+
+  it('should handle v', () => {
+    const current = new Current()
+    for (let i = 0; i < 13; i++)
+      current.move(map.findNextPoints(current.point()))
+    expect(current.point()).toEqual({ x: 3, y: 3 })
+
+    current.move(map.findNextPoints(current.point()))
+    expect(current.point()).toEqual({ x: 3, y: 5 })
   })
 })
--- puzzles/day23/part1/part1.ts
+++ puzzles/day23/part1/part1.ts
@@ -1,4 +1,120 @@
+type MapValues = '.' | 'v' | '>' | '#'
+
+type MapItem = {
+  point: Point
+  value: MapValues
+}
+
+export type Point = {
+  x: number
+  y: number
+}
+
 export const day23Part1 = (input: string) => {
-  const lines = input.split('\n').filter((x) => !!x)
-  return `Ready, solve that puzzle! Input: ${lines}`
+  const randomHikeStep: number[] = []
+
+  for (let i = 0; i < 50; i++) {
+    const maze = new MazeMap(input)
+    const current = new Current()
+    while (current.hiking()) {
+      const nextPoints = maze.findNextPoints(current.point())
+      current.move(nextPoints)
+    }
+    randomHikeStep.push(current.steps())
+  }
+
+  return Math.max(...randomHikeStep)
+}
+
+export class MazeMap {
+  lines: string[]
+
+  constructor(input: string) {
+    const lines = input.split('\n').filter((x) => !!x)
+    this.lines = lines
+  }
+
+  getItem = (point: Point): MapValues | null => {
+    if (!this.lines[point.y]) return null
+    return this.lines[point.y][point.x] as MapValues
+  }
+
+  findNextPoints = (currentPoint: Point): MapItem[] => {
+    const down = { ...currentPoint, y: currentPoint.y + 1 }
+    const up = { ...currentPoint, y: currentPoint.y - 1 }
+    const right = { ...currentPoint, x: currentPoint.x + 1 }
+    const left = { ...currentPoint, x: currentPoint.x - 1 }
+    return [down, up, right, left]
+      .map((point) => {
+        const value = this.getItem(point)
+        if (value && value !== '#') return { value, point }
+      })
+      .filter((x) => x !== undefined) as MapItem[]
+  }
+}
+
+export class Current {
+  private pointHistory: Point[] = [{ x: 1, y: 0 }]
+  private isHiking = true
+
+  point = () => this.pointHistory[this.pointHistory.length - 1]
+  move = (items: MapItem[]) => {
+    const validNextMoves = this.findValidMoves(items)
+
+    if (validNextMoves.length === 0) this.isHiking = false
+
+    if (validNextMoves.length === 1) {
+      const move = validNextMoves[0]
+      this.pointHistory.push(move.point)
+      this.skipWhenV(move)
+      this.skipWhenRightArrow(move)
+    }
+
+    if (validNextMoves.length > 1) {
+      const randomIndex = Math.floor(Math.random() * validNextMoves.length)
+      const selectedMove = validNextMoves[randomIndex]
+      this.pointHistory.push(selectedMove.point)
+      this.skipWhenV(selectedMove)
+      this.skipWhenRightArrow(selectedMove)
+    }
+  }
+
+  hiking = () => this.isHiking
+  steps = () => this.pointHistory.length - 1
+
+  private findValidMoves = (items: MapItem[]) => {
+    const validNextMoves: MapItem[] = []
+    items.forEach((item) => {
+      const existingItem = this.pointHistory.find(
+        (point) => point.x === item.point.x && point.y === item.point.y
+      )
+
+      const isGoingUpOnV = item.value === 'v' && item.point.y < this.point().y
+
+      const isGoingLeftOnRightArrow =
+        item.value === '>' && item.point.x < this.point().x
+      if (!existingItem && !isGoingUpOnV && !isGoingLeftOnRightArrow) {
+        validNextMoves.push(item)
+      }
+    })
+    return validNextMoves
+  }
+
+  private skipWhenV = (item: MapItem) => {
+    if (item.value === 'v') {
+      this.pointHistory.push({
+        ...item.point,
+        y: item.point.y + 1,
+      })
+    }
+  }
+
+  private skipWhenRightArrow = (item: MapItem) => {
+    if (item.value === '>') {
+      this.pointHistory.push({
+        ...item.point,
+        x: item.point.x + 1,
+      })
+    }
+  }
 }
